c70f8ebcd07fa86ca7b4c1316beeb10d51f83fe1
RohitRaj766 <rohitraj2k04@gmail.com>
2024-12-10 21:10:01 +0530

    Developer roadmap img to component mobile

--- src/lib/components/academy/RoadmapSection.svelte
+++ src/lib/components/academy/RoadmapSection.svelte
@@ -1,7 +1,56 @@
-  <!-- Roadmap Section  -->
-<div class="sm:hidden">
-    <img class="mx-auto w-full" src="/academy/roadmap-mobile-1.svg" alt="">
-  </div>
+
+   
+   <!--Mobile-->
+    <div class="sm:hidden w-full h-full bg-repeat py-4" style="background-image: url('/academy/roadmap-mobile-1.svg');">
+      <!-- Content inside the div if needed -->
+       <div class="flex justify-center items-center w-full mt-10">
+        <div class="flex flex-col items-center justify-center">
+          <img src="/academy/bytcl.svg" alt="" class="w-[60%]">
+          <p class="text-2xl text-center text-[#FFFFFF] mt-6">Blockchain Developer  <br>
+            Roadmap</p>
+          </div>
+       </div>
+       <div class="w-full flex justify-around items-center px-4 mt-10 gap-4">
+        <div class="flex flex-col justify-center items-center">
+          <img src="/academy/roadmapm1.svg" alt="" >
+          <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 1</p>
+          <p class="text-center text-[#FFFFFF] text-sm mt-4">Learn the basics of  blockchain technology</p>
+        </div>
+        
+        <div class="flex flex-col justify-center items-center">
+              <img src="/academy/roadmapm2.svg" alt="">
+              <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 2</p>
+              <p class="text-center text-[#FFFFFF] text-sm mt-4">Get a grip on the frontend development</p>
+            </div> 
+       </div>
+
+       <div class="w-full flex justify-around mt-10 px-4 gap-4">
+        <div class="flex flex-col justify-center items-center">
+          <img src="/academy/roadmapm3.svg" alt="">
+          <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 3</p>
+          <p class="text-center text-[#FFFFFF] text-sm mt-4">Dive into backend development to build scalable blockchain solutions.</p>
+        </div> 
+        <div class="flex flex-col justify-center items-center">
+          <img src="/academy/roadmapm4.svg" alt="">
+          <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 4</p>
+          <p class="text-center text-[#FFFFFF] text-sm mt-4">Know the roadmap to build applications loved by users</p>
+        </div> 
+   </div>
+
+   <div class="w-full flex justify-center mt-10 mb-4 px-4">
+    <div class="flex flex-col justify-center items-center">
+      <img src="/academy/roadmapm5.svg" alt="">
+      <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 5</p>
+      <p class="text-center text-[#FFFFFF] text-sm mt-4">Crack the interview with <br> placement preparation</p>
+    </div> 
+   </div>
+   
+</div>
+    
+
+  <!--Desktop-->
 <div class="hidden sm:flex justify-center max-w-[1440px] mx-auto">
     <img class="mx-auto w-full" src="/academy/roadmap-section-1.svg" alt="">
   </div>
+
+
