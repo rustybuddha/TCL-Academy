81c7204f875d8a71c76205799b1f2ca3e4595c00
RohitRaj766 <rohitraj2k04@gmail.com>
2024-12-10 22:08:46 +0530

    Devloper Roadmap Desktop and responsiveness done

--- src/lib/components/academy/RoadmapSection.svelte
+++ src/lib/components/academy/RoadmapSection.svelte
@@ -46,11 +46,43 @@
    </div>
    
 </div>
-    
-
   <!--Desktop-->
-<div class="hidden sm:flex justify-center max-w-[1440px] mx-auto">
-    <img class="mx-auto w-full" src="/academy/roadmap-section-1.svg" alt="">
+  <div class="hidden sm:flex flex-col justify-center max-w-[1440px] h-[450px] mx-auto bg-cover bg-center" style="background-image: url('/academy/roadmap-section-1.svg');">
+   
+    <div class="flex justify-center items-center">
+      <div class="flex justify-between items-center w-[90%]">
+        <p class="text-2xl md:text-3xl lg:text-4xl text-[#FFFFFF]">Blockchain Developer Roadmap</p>
+          <img src="/academy/bytcl.svg" alt="" class="w-[180px]">
+        </div>
+     </div>
+     
+     <div class="w-[90%] flex justify-start items-start mt-[60px] mb-4  gap-10  m-auto ">
+      <div class="flex flex-col justify-center items-start  ">
+        <img src="/academy/roadmapf1.svg" alt="" class="w-[70%]">
+        <p class="text-[#FFFFFF] md:text-lg lg:text-xl mt-4">Step 1</p>
+        <p class="text-[#EBEBEB] md:text-sm lg:text-[16px]  mt-4 h-[100px] font-[300]">Learn the basics of blockchain <br> technology</p>
+      </div> 
+      <div class="flex flex-col justify-center items-start">
+        <img src="/academy/roadmapf2.svg" alt="" class="w-[70%]">
+        <p class="text-[#FFFFFF] md:text-lg lg:text-xl mt-4">Step 2</p>
+        <p class="text-[#EBEBEB] md:text-sm lg:text-[16px]  mt-4 h-[100px] font-[300]">Get a grip on the frontend <br> development</p>
+      </div> 
+      <div class="flex flex-col justify-center items-start">
+        <img src="/academy/roadmapf3.svg" alt="" class="w-[70%]">
+        <p class="text-[#FFFFFF] md:text-lg lg:text-xl mt-4">Step 3</p>
+        <p class="text-[#EBEBEB] md:text-sm lg:text-[16px]  mt-4 h-[100px] font-[300]">Dive into backend <br> development to build scalable <br> blockchain solutions.</p>
+      </div> 
+      <div class="flex flex-col justify-center items-start">
+        <img src="/academy/roadmapf4.svg" alt="" class="w-[70%]">
+        <p class="text-[#FFFFFF] md:text-lg lg:text-xl mt-4">Step 4</p>
+        <p class="text-[#EBEBEB] md:text-sm lg:text-[16px]  mt-4 h-[100px] font-[300]">Know the roadmap to build <br> applications loved by users</p>
+      </div> 
+      <div class="flex flex-col justify-center items-start">
+        <img src="/academy/roadmapf5.svg" alt="" class="w-[15%]">
+        <p class="text-[#FFFFFF] md:text-lg lg:text-xl mt-4">Step 5</p>
+        <p class="text-[#EBEBEB] md:text-sm lg:text-[16px]  mt-4 h-[100px] font-[300]">Crack the interview with <br> placement preparation</p>
+      </div> 
+     </div>
   </div>
 
 
