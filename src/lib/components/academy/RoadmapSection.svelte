63f72edd63d6f8b64042046d148757fbd0640288
RohitRaj766 <rohitraj2k04@gmail.com>
2024-12-13 08:59:17 +0530

    Roadmap New Design mobile version added

--- src/lib/components/academy/RoadmapSection.svelte
+++ src/lib/components/academy/RoadmapSection.svelte
@@ -1,50 +1,90 @@
 
    
    <!--Mobile-->
-    <div class="sm:hidden w-full h-full bg-repeat py-4" style="background-image: url('/academy/roadmap-mobile-1.svg');">
-     <div class="flex justify-center items-center w-full mt-10">
-        <div class="flex flex-col items-center justify-center">
-          <img src="/academy/bytcl.svg" alt="" class="w-[60%]">
-          <p class="text-2xl text-center text-[#FFFFFF] mt-6">Blockchain Developer  <br>
-            Roadmap</p>
+    <div class="sm:hidden w-full h-full bg-repeat py-[3.75rem]" style="background-image: url('/academy/newroadmapbg.svg');">
+      <div class=" w-[100%] flex flex-col">
+        <p class="font-[500] font-[Bai Jamjuree] font-[#333333] text-3xl text-center" style="line-height: 38px;">Roadmap to Master <br>
+          Blockchain Skills</p>
+          <div class="flex justify-center items-center mt-3">
+            <p class="font-['Rubik'] text-[1rem] font-[400] transform rotate-[-2deg]">Duration</p>
+            <img src="/academy/roadmappin.svg" alt="pin" class="ml-3">
           </div>
-       </div>
-       <div class="w-full flex justify-around items-center px-4 mt-10 gap-4">
-        <div class="flex flex-col justify-center items-center">
-          <img src="/academy/roadmapm1.svg" alt="" >
-          <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 1</p>
-          <p class="text-center text-[#FFFFFF] text-sm mt-4">Learn the basics of  blockchain technology</p>
         </div>
-        
-        <div class="flex flex-col justify-center items-center">
-              <img src="/academy/roadmapm2.svg" alt="">
-              <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 2</p>
-              <p class="text-center text-[#FFFFFF] text-sm mt-4">Get a grip on the frontend development</p>
-            </div> 
-       </div>
 
-       <div class="w-full flex justify-around mt-10 px-4 gap-4">
-        <div class="flex flex-col justify-center items-center">
-          <img src="/academy/roadmapm3.svg" alt="">
-          <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 3</p>
-          <p class="text-center text-[#FFFFFF] text-sm mt-4">Dive into backend development to build scalable blockchain solutions.</p>
-        </div> 
-        <div class="flex flex-col justify-center items-center">
-          <img src="/academy/roadmapm4.svg" alt="">
-          <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 4</p>
-          <p class="text-center text-[#FFFFFF] text-sm mt-4">Know the roadmap to build applications loved by users</p>
-        </div> 
-   </div>
+        <div class="flex flex-col justify-center items-center mt-6">
+<!-- 1st card -->
+          <div class="flex flex-col px-2 justify-center items-center max-w-[313px] border-b pb-5">
+            <img src="/academy/newroadmapitem1.svg" alt="newroadmapitem">
+            <p class="font-['Rubik'] font-[400] text-sm text-left mt-5">Learn blockchain basics and build a project to <br> fortify your basics.</p>
+            <ul class="list-disc list-inside w-full mt-5">
+              <li class="font-['Rubik'] !font-[400] text-sm text-left">Wallet Integration with Frontend</li>
+              <li class="font-['Rubik'] !font-[400] text-sm text-left ">Portfolio Project</li>
+            </ul>
+            
+            <div class="mt-5 flex justify-start items-center w-full">
+              <p class="font-['Rubik'] font-[400] text-sm text-left">Duration:</p>
+              <img src="/academy/2weeks1.svg" alt="" class="ml-2">
+            </div>
+          </div>
+<!-- 2nd card -->
+          <div class="flex flex-col px-2 justify-center items-center max-w-[308px] border-b pb-5 mt-6">
+            <img src="/academy/roadmapitem2.svg" alt="newroadmapitem">
+            <p class="font-['Rubik'] font-[400] text-sm text-left mt-5 -ml-10">Get a grip on frontend and backend <br> technologies and learn how to deploy a <br> fullstack application.</p>
+            <ul class="list-disc list-inside w-full mt-5">
+              <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.8px]">
+                <span class="!font-[500]">Frontend</span> - React, Next, Svelte, and Svelte Kit
+              </li>
+              <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.8px]">
+                <span class="!font-[500]">Backend</span> - Nest JS, GoLang, Java, Express JS
+              </li>
+            </ul>
 
-   <div class="w-full flex justify-center mt-10 mb-4 px-4">
-    <div class="flex flex-col justify-center items-center">
-      <img src="/academy/roadmapm5.svg" alt="">
-      <p class="text-center text-[#FFFFFF] text-lg mt-4">Step 5</p>
-      <p class="text-center text-[#FFFFFF] text-sm mt-4">Crack the interview with <br> placement preparation</p>
-    </div> 
-   </div>
-   
+            <div class="w-full flex flex-col mt-5 ">
+              <p class="font-['Rubik'] font-[500] text-sm text-left">Fullstack Development</p>
+              <div class="mt-2 flex -ml-[9px]">
+                <img src="/academy/fullstackdev.svg" alt="">
+              </div>
+            </div>
+            
+            <div class="mt-5 flex justify-start items-center w-full">
+              <p class="font-['Rubik'] font-[400] text-sm text-left">Duration:</p>
+              <img src="/academy/2weeks2.svg" alt="" class="ml-2">
+            </div>
+          </div>
+<!-- 3rd card -->
+<div class="flex flex-col px-2 justify-center items-center max-w-[313px] border-b pb-5 mt-6">
+  <img src="/academy/roadmap3.svg" alt="newroadmapitem">
+  <p class="font-['Rubik'] font-[400] text-sm text-left mt-5">Dive deeply into the crux of blockchain <br> technology and explore how to deploy smart <br> contracts and build architecture for scalable <br> blockchain applications.</p>
+  <ul class="list-disc list-inside w-full mt-5">
+    <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.6px]">Comparative Analysis of Blockchain Networks</li>
+    <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.6px]">Tokenomics</li>
+    <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.6px]">Security Token Contracts and Smart Contract</li>
+    <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.6px]">Data architecture for onchain applications</li>
+  </ul>
+  
+  <div class="mt-5 flex justify-start items-center w-full">
+    <p class="font-['Rubik'] font-[400] text-sm text-left">Duration:</p>
+    <img src="/academy/2weeks3.svg" alt="" class="ml-2">
+  </div>
 </div>
+<!-- 4th card -->
+<div class="flex flex-col px-2 justify-center items-center max-w-[313px] pb-5 mt-6">
+  <img src="/academy/roadmap4.svg" alt="newroadmapitem">
+  <p class="font-['Rubik'] font-[400] text-sm text-left mt-5">Build your proof of work and be placement <br> ready with resume review, mock interviews <br> and feedback.</p>
+  <ul class="list-disc list-inside w-full mt-5">
+    <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.6px]">Wallet Integration with Frontend</li>
+    <li class="font-['Rubik'] !font-[400] text-sm text-left tracking-[-0.6px]">Portfolio Project</li>
+  </ul>
+  
+  <div class="mt-5 flex justify-start items-center w-full">
+    <!-- <p class="font-['Rubik'] font-[400] text-sm text-left">Duration:</p> -->
+    <img src="/academy/roadmap4item.svg" alt="" class="">
+  </div>
+</div>
+
+        </div>
+       
+    </div>
   <!--Desktop-->
   <div class="hidden sm:flex flex-col justify-center max-w-[1440px] h-[450px] mx-auto bg-cover bg-center" style="background-image: url('/academy/roadmap-section-1.svg');">
    
