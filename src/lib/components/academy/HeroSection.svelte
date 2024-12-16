aeafe3bbfe06a21f65820b3bf0a4762f9879c569
RohitRaj766 <rohitraj2k04@gmail.com>
2024-12-16 19:57:52 +0530

    word breaking fix hero section

--- src/lib/components/academy/HeroSection.svelte
+++ src/lib/components/academy/HeroSection.svelte
@@ -26,7 +26,7 @@
   <div class="mx-auto">
     <div class="w-full m-auto">
       <div class="flex items-center justify-center m-auto relative px-5">
-        <span class="text-xl md:text-[24px] text-center !font-[500]">
+        <span class="text-xl text md:text-[24px] text-center !font-[500]">
           Master Blockchain Development<br> and Boost your Experience <br>
           with Projects and <br> Internships
           <p class="flex justify-center">
@@ -216,3 +216,11 @@
     <img class="mr-[5%] max-w-full h-auto" src="/academy/herosection-Rohan.svg" alt="img" loading="lazy">
   </div>
 </div>
+
+<style>
+  @media(max-width:360px){
+    .text{
+      font-size: 16px !important;
+    }
+  }
+</style>
