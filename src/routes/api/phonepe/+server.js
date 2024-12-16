35a018d0ef61b57a6b1f1904972ddb1fe7b4a6b5
Riteek Rakesh <riteek7.12@gmail.com>
2024-12-16 15:45:35 +0530

    fixing checksum

--- src/routes/api/phonepe/+server.js
+++ src/routes/api/phonepe/+server.js
@@ -42,7 +42,7 @@ export const POST = async ({ request }) => {
         }
 
         // Step 6: Calculate checksum
-        const checksumInput = callbackData.response + "9f37e36f-b58f-4e34-9a28-cb7fdd686d0" //20974b1d-ae1c-42f5-9685-22d6c8da9dc7// Prod key  //'9f37e36f-b58f-4e34-9a28-cb7fdd686d0' test key 
+        const checksumInput = callbackData.response + "96434309-7796-489d-8924-ab56988a6076" //20974b1d-ae1c-42f5-9685-22d6c8da9dc7// Prod key  //'96434309-7796-489d-8924-ab56988a6076' test key 
         const calculatedChecksum = sha256(checksumInput);
         console.log("Step 6: Calculated checksum:", calculatedChecksum);
 
