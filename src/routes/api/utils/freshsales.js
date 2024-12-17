9e3bdc88a884a068b8c617307e09f07dc2c6636d
Riteek Rakesh <riteek7.12@gmail.com>
2024-12-17 11:32:52 +0530

    added contact and product

--- src/routes/api/utils/freshsales.js
+++ src/routes/api/utils/freshsales.js
@@ -1,52 +1,56 @@
-export async function createContact(fullname, email, linkedin, address, mobile_number) {
+export async function createContact(fullname, email, linkedin, address, mobile_number, profession, organization, referedBy, country) {
     const url = "https://tclabs.myfreshworks.com/crm/sales/api/contacts";
-  
+
     // Split the fullname into first and last names
     const [first_name, ...lastNameParts] = fullname.split(" ");
     const last_name = lastNameParts.join(" ");
 
     console.log("error catch 1")
-  
+
     const payload = {
-      contact: {
-        first_name: first_name ,
-        last_name: last_name,
-        email: email,
-        linkedin: linkedin,
-        address: address,
-        mobile_number: mobile_number,
-      },
+        contact: {
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            linkedin: linkedin,
+            address: address,
+            mobile_number: mobile_number,
+            job_title: profession ,
+            keyword: organization,
+            medium: referedBy,
+            country: country,
+        },
     };
 
     console.log(payload)
-  
+
     try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Authorization": "Token token=t6MwZk-wEKklzOlJmcIWGA",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const data = await response.json();
-      console.log("Response data:", data);
-  
-      // Return the created contact data
-      return data.contact.id;
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Authorization": "Token token=t6MwZk-wEKklzOlJmcIWGA",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Response data:", data);
+
+        // Return the created contact data
+        return data.contact.id;
     } catch (error) {
-      console.error("Error while creating the contact:", error);
+        console.error("Error while creating the contact:", error);
     }
-  }
+}
 
-export async function createDeal(name, email, contactNumber, linkedIN, address, countryName) {
+export async function createDeal(name, email, contactNumber, linkedIN, address, countryName, profession, organization, referedBy) {
 
-    const contact_id = await createContact(name, email, linkedIN, address, contactNumber)
+    const contact_id = await createContact(name, email, linkedIN, address, contactNumber, profession, organization, referedBy, countryName)
     const url = "https://tclabs.myfreshworks.com/crm/sales/api/deals";
 
     const payload = {
@@ -60,9 +64,16 @@ export async function createDeal(name, email, contactNumber, linkedIN, address, 
                 address: address,
                 country: countryName,
             },
-            "currency_id":402000190124, 
-            "deal_pipeline_id": 402000189098,    
-            "contacts_added_list" : [contact_id],
+            "currency_id": 402000190124,
+            "deal_pipeline_id": 402000189098,
+            "contacts_added_list": [contact_id],
+            "products": [
+                {
+                    "id": 402000768605,
+                    "quantity": 1,
+                    "unit_price": 8999
+                }
+            ]
         },
     };
 
